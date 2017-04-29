14f1a30638ac280e42e41511fdc06907f94364e4
nbuhay <nbuhay@gmail.com>
2017-04-29 06:04:50 -0400

    Add gulp automation to build deployable .zip.  Add Developer Skill interface assets.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 var gulp = require('gulp');
-var clean = require('gulp-clean');
+var filter = require('gulp-filter');
 var zip = require('gulp-zip');
-
-var folder = '**';
 
 gulp.task('default', () => {
 	console.log('test');
 });
 
 gulp.task('deploy', () => {
-	return gulp.src(folder)
+
+	// include all files and folders in build 
+	const folder = '**';
+	// include all files that start with a . i.e. .config, .gitignore, etc.
+	const opts = { nodir: true, dot: true };
+	// exclude existing .zip from this build - it will be overwritten in the pipe
+	const f = filter(['**', '!deploy/*.zip']);
+
+	return gulp.src(folder, opts)
+		.pipe(f)
 		.pipe(zip('hello.zip'))
 		.pipe(gulp.dest('./deploy'));
 });
